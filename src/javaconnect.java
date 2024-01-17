a0ce6519f86c1d2cdf84451e081d8da8cf7bb106
@JohnVelLlacuna <>
2024-01-17 00:37:55 -0800

    Bank System Project

---
+++ src/javaconnect.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Win10
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+    Connection conn = null;
+    
+    
+    public static Connection Connecrdb(){
+       try{
+           Class.forName("org.sqlite.JDBC");
+           Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Win10\\Documents\\NetBeansProjects\\BankSystess\\bank.db");
+           return conn;
+       }catch(Exception e){
+           JOptionPane.showMessageDialog(null, e);
+       }
+       return null;
+    }
+    
+}
